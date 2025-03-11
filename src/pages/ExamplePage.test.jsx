0a52e13465212923353701cfef6a5e52c0abff61
Rajesh D <rajesh@Rajeshs-MacBook-Pro.local>
2025-03-11 14:11:42 +0530

    Setup Jest & React Testing Library with Vitest

---
+++ src/pages/ExamplePage.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+
+import ExamplePage from './ExamplePage';
+
+describe('ExamplePage component', () => {
+    it('should render text - React JS Application', () => {
+        render(<ExamplePage />);
+
+        expect(screen.getByText('React JS Application')).toBeInTheDocument();
+    });
+});
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true, // Enables global test functions like describe, it, test, beforeEach, and afterEach without having to import them in every test file
+    environment: 'jsdom', // Specifies that tests should run in a JSDOM environment instead of Node.js
+    setupFiles: './setupTests.js' // Ensures that necessary configurations (like extending Jest matchers with @testing-library/jest-dom) are applied globally
+  },
 });
